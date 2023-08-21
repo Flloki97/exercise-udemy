c74c774a4902d82e78eb79d49aa1b5a04abd2eca
Florent <fbryma7@gmail.com>
2023-08-21 15:38:32 +0200

    adding some changes to js

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-// function bmiCalculator(weight, height) {
-//     var bmi = weight / Math.pow(height, 2);
-//     return Math.round(bmi);
-// }
+function bmiCalculator(weight, height) {
+    var bmi = weight / Math.pow(height, 2);
+    return Math.round(bmi);
+}
 
-// var bmi = bmiCalculator(95, 1.8);
-// console.log(bmi);
-
-
-// function randomNumber() {
-//     var n = Math.random();
-//     n = n * 6;
-//     n = Math.floor(n) + 1;
-//     return n;
-// }
-
-// console.log(randomNumber());
+var bmi = bmiCalculator(95, 1.8);
+console.log(bmi);
 
 
+function randomNumber() {
+    var n = Math.random();
+    n = n * 6;
+    n = Math.floor(n) + 1;
+    return n;
+}
+
+console.log(randomNumber());
 
 
 
-// function loveMeter() {
-//     var yourName = prompt("Whats your name?");
-//     var theirName = prompt("Whats their name?");
+
+
+function loveMeter() {
+    var yourName = prompt("Whats your name?");
+    var theirName = prompt("Whats their name?");
     
-//     var loveScore = Math.random() * 100;
-//     loveScore = Math.floor(loveScore) + 1;
+    var loveScore = Math.random() * 100;
+    loveScore = Math.floor(loveScore) + 1;
 
-//     if(loveScore >= 90) {
-//         alert(yourName + " " + "loves " + theirName + " " + loveScore + "%" + "You love each other like Kanye loves Kanye");
-//     } else {
-//         alert(yourName + " " + "loves " + theirName + " " + loveScore + "%");
-//     }
-// }
+    if(loveScore >= 90) {
+        alert(yourName + " " + "loves " + theirName + " " + loveScore + "%" + "You love each other like Kanye loves Kanye");
+    } else {
+        alert(yourName + " " + "loves " + theirName + " " + loveScore + "%");
+    }
+}
 
-// console.log(loveMeter());
+console.log(loveMeter());
 
 
 var weight = prompt("What is your weight?");
